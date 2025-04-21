a087f317321e4d1180241aca2db452db403bd2ea
josedrozd <drozd.jose@gmail.com>
2025-04-20 21:15:59 -0300

    ng build

--- src/main/resources/static/server/angular-app-manifest.mjs
+++ src/main/resources/static/server/angular-app-manifest.mjs
@@ -8,14 +8,14 @@ export default {
   entryPointToBrowserMapping: {
   "src/app/components/mp-checkout/mp-checkout.component.ts": [
     {
-      "path": "chunk-6HIKCHLT.js",
+      "path": "chunk-65H6AVKV.js",
       "dynamicImport": false
     }
   ]
 },
   assets: {
-    'index.csr.html': {size: 10911, hash: '4d2c53245e022305437510737be78a95f392e0b299cd41f316d9c5e04fe69c9e', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 6779, hash: '09b31cd7479626d1b0f92ed19a023d5a67dae8b2be42fd787177c6f514b378b1', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 10911, hash: '476475479be091665d695190384152f32109e71cac09ed9c46e90864fe1e3965', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 6779, hash: '9e8a9fd245031d6f0f4a7feac70e7d24bd48af278972bcb29e395a89996e73d2', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-LPWYHEJT.css': {size: 231803, hash: 'EIu70z819oI', text: () => import('./assets-chunks/styles-LPWYHEJT_css.mjs').then(m => m.default)}
   },
 };
