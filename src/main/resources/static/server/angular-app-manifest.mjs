1ca8a0e60d541457dabf20e452b9710b99ebe824
josedrozd <drozd.jose@gmail.com>
2025-04-21 05:49:34 -0300

    add ssl to call backend urls

--- src/main/resources/static/server/angular-app-manifest.mjs
+++ src/main/resources/static/server/angular-app-manifest.mjs
@@ -8,14 +8,14 @@ export default {
   entryPointToBrowserMapping: {
   "src/app/components/mp-checkout/mp-checkout.component.ts": [
     {
-      "path": "chunk-65H6AVKV.js",
+      "path": "chunk-ZUGFMLVB.js",
       "dynamicImport": false
     }
   ]
 },
   assets: {
-    'index.csr.html': {size: 10911, hash: '476475479be091665d695190384152f32109e71cac09ed9c46e90864fe1e3965', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 6779, hash: '9e8a9fd245031d6f0f4a7feac70e7d24bd48af278972bcb29e395a89996e73d2', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 10911, hash: '97c9672d308ef01ba7b36e48b4c5b352133b7c6ae6aaea952546fe21dab4315f', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 6779, hash: '311bc69033a6f2196b2a9af326d563c41be014caa7fa4b3024575b13c8b5b831', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-LPWYHEJT.css': {size: 231803, hash: 'EIu70z819oI', text: () => import('./assets-chunks/styles-LPWYHEJT_css.mjs').then(m => m.default)}
   },
 };
