f1c2b817c6d58ea1416db5c526f63e0fc26d7e7c
josedrozd <drozd.jose@gmail.com>
2025-04-21 08:48:30 -0300

    change url format to show images from proxy

--- src/main/resources/static/server/angular-app-manifest.mjs
+++ src/main/resources/static/server/angular-app-manifest.mjs
@@ -8,14 +8,14 @@ export default {
   entryPointToBrowserMapping: {
   "src/app/components/mp-checkout/mp-checkout.component.ts": [
     {
-      "path": "chunk-B7YGPDA2.js",
+      "path": "chunk-5JRHWUPJ.js",
       "dynamicImport": false
     }
   ]
 },
   assets: {
-    'index.csr.html': {size: 10911, hash: '90a4f1f490ab1b8ba7e12ed868b95caee983a5c6bc2b53350e170801e4e13653', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 6779, hash: 'd8c067555ce24b423dc628c0c68a53dd90f18a460fa3e0c76545c8e8f5ce9cee', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 10911, hash: '2b6c6ea37561d8a5af639b1c02f53f3da899b77402788c1784a4ad670ce9ccb2', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 6779, hash: '62131211b51bc5305702accb8e658a34b0e689a2f7769fc6b8af2311733d6bcf', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-LPWYHEJT.css': {size: 231803, hash: 'EIu70z819oI', text: () => import('./assets-chunks/styles-LPWYHEJT_css.mjs').then(m => m.default)}
   },
 };
