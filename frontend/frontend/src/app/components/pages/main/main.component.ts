6e2e830b3cd2a917fe3fa0ccff1118900f24091a
josedrozd <drozd.jose@gmail.com>
2025-05-23 00:31:26 -0300

    building main page

--- frontend/frontend/src/app/components/pages/main/main.component.ts
+++ frontend/frontend/src/app/components/pages/main/main.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../header/header.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-main',
   imports: [
+    RouterModule
   ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
