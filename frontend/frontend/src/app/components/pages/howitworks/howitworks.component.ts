3fb2fbcbd5ed75af8b16ccb623329d4a62f8e257
josedrozd <drozd.jose@gmail.com>
2025-05-25 01:43:04 -0300

    add promotion and products components

---
+++ frontend/frontend/src/app/components/pages/howitworks/howitworks.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-howitworks',
+  imports: [],
+  templateUrl: './howitworks.component.html',
+  styleUrl: './howitworks.component.css'
+})
+export class HowitworksComponent {
+
+}
